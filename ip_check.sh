b0db9f4ca9993b7fec2c4f751eae5c1339d30bda
edilprt <61586818+edilprt@users.noreply.github.com>
2020-02-28 18:55:04 +0600

    Update ip_check.sh

--- ip_check.sh
+++ ip_check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ip=$(wget -qO- ipinfo.io/ip)
+ip=$(wget -qO- ipinfo.io/ip);
   while :; do
   sleep 60
-  cip=$(wget -qO- ipinfo.io/ip)
+  cip=$(wget -qO- ipinfo.io/ip);
      if [[ $ip != $cip ]]; then 
      echo "miner restart"
      fi
