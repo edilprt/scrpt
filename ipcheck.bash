51ef2ee70530f8d377416274accbd22b38c81eb1
edilprt <61586818+edilprt@users.noreply.github.com>
2020-03-02 18:12:53 +0600

    Update ipcheck.bash
    Добавил уведомления в dashboard

--- ipcheck.bash
+++ ipcheck.bash
@@ -5,15 +5,18 @@
 #!/bin/bash
 ip=$(wget -qO- ipinfo.io/ip);
 echo "Текущий IP $ip";
+ct=$(date +%T);
+message info "$ct - IP check успешно загружен. Текущий IP: $ip";
   while :; do
  # sleep 60
   cip=$(wget -qO- ipinfo.io/ip);
   d=$(date +%T);
      if [  $ip != $cip  ]; then 
-     echo  ["$d: IP изменился. Перезагрузка";
+     echo  "$d: IP изменился. Перезагрузка";
+     message info "$d IP изменился. Перезагружаю риг."
      shutdown -r;
      else
-     echo ["$d: IP не изменился. Проверка повторится через 60 сек...]";
+     echo "$d: IP не изменился. Проверка повторится через 60 сек...";
      sleep 60;
      fi
   done
