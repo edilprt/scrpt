b472754718d60da2337bfb92033e9f2e102f1eaa
edilprt <61586818+edilprt@users.noreply.github.com>
2020-03-02 18:18:06 +0600

    Update ipcheck.bash
    исправил "shutdown -r" на "restart"

--- ipcheck.bash
+++ ipcheck.bash
@@ -14,7 +14,7 @@ message info "$ct - IP check успешно загружен. Текущий IP:
      if [  $ip != $cip  ]; then 
      echo  "$d: IP изменился. Перезагрузка";
      message info "$d IP изменился. Перезагружаю риг."
-     shutdown -r;
+     restart;
      else
      echo "$d: IP не изменился. Проверка повторится через 60 сек...";
      sleep 60;
